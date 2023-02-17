9c9a4f1cb040ff1b76746a296323693d5856938f
ProfesionalKillr <santipiza421@gmail.com>
2023-02-17 11:30:06 -0500

    Cambios en el codigo de promedio

--- src/promedio.java
+++ src/promedio.java
@@ -12,12 +12,12 @@ public class promedio {
         System.out.println("Ahora digite el promedio de asistencia: ");
         double P_notas=Nota1+Nota2+Nota3/3;
         P_Asistencia = lectura.nextDouble();
-        if ( P_notas>=35 && P_Asistencia>=70 )
+        if ( P_notas>=3.5 && P_Asistencia>=70 )
         {
-            System.out.println("Aprobo :D!");
+            System.out.println("Su promedio de notas fue :" + P_notas +" Y su porcentaje de asistencia fue: " + P_Asistencia + " Felicitaciones Aprobo!");
         }
         else {
-            System.out.println("No Aprobo D:");
+            System.out.println("Su promedio de notas fue :" + P_notas +" Y su porcentaje de asistencia fue: " + P_Asistencia + " Desafortunadamente no Aprobo");
         }
         lectura.close();
     }
