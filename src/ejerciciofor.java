29022d38ba315acd4dd4454912acf4c69fd485b7
ProfesionalKillr <santipiza421@gmail.com>
2023-02-21 11:23:45 -0500

    se modifico moneda, y se agrego reto2 (incompleto), ejerciciofor

---
+++ src/ejerciciofor.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class ejerciciofor {
+    public static void main(String[] args) {
+        int precio,cantidad,contador,subtotal,acumulador,billete, vueltas;
+        String telefonia;
+        contador=0;
+        acumulador=0;
+        Scanner leer=new Scanner (System.in);
+        for(int x=1; x<=5; x++)
+        {
+            System.out.println("Digite el precio del producto: ");
+            precio=leer.nextInt();
+            System.out.println("Digite la cantidad del producto: ");
+            cantidad=leer.nextInt();
+            contador=contador+1;
+            subtotal=precio*cantidad;
+            acumulador=acumulador+subtotal;
+            
+        }
+        System.out.println("El valor total a pagar es: " + acumulador);
+        System.out.println("Billete con el que va a pagar: ");
+        billete=leer.nextInt();
+        vueltas=billete-acumulador;
+        System.out.println("Sus vueltas son: " + vueltas + "¿Cuenta con telefonia movil exito?");
+        telefonia=leer.next();
+        if (telefonia.equals("si")){
+            System.out.println("Los minutos adquiridos en su compra son: " + contador);
+        }
+        else{
+            System.out.println("No pierdas mas minutos, adquiere ya tu telefonia movil exito");
+        }
+        leer.close();
+    }
+}
