5fb46a34c3734e8b79b74e91db69304f0dab0973
ProfesionalKillr <santipiza421@gmail.com>
2023-02-21 09:08:50 -0500

    se realizaron correcciones a moneda/reto

--- src/moneda.java
+++ src/moneda.java
@@ -1,18 +1,24 @@
-import java.util.Scanner;
+
 import java.util.Random;
+import java.util.Scanner;
 public class moneda {
     public static void main(String[] args) throws Exception {
-        Scanner lectura = new Scanner (System.in);
-        System.out.println("Cara o Sello:");
-        lectura.nextInt();
+        Scanner lectura=new Scanner (System.in);
+        Random aleatorio=new Random ();
+        boolean cara_cruz;
+        String opcion;
+        System.out.println("digite cara o sello");
+        opcion=lectura.nextLine();
+        cara_cruz=aleatorio.nextBoolean();
         lectura.close();
-        Random numAleatorio = new Random();
-        boolean moneda = numAleatorio.nextBoolean();
-        if (moneda) {
-            System.out.println("Cara");
-        } else {
-            System.out.println("Sello");
+        if(opcion.equals("cara")&& cara_cruz==true){
+            System.out.println("Usted ha ganado al escoger cara");
         }
-        
+        else if (opcion.equals("sello") && cara_cruz==false){
+            System.out.println("Usted ha ganado al escoger sello");
+        }
+        else{
+            System.out.println("Usted ha perdido");
+        }
     }
 }
